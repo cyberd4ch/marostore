002fa4728d275f9b078b51d7ccd9c9a33d66023b
dach-a <dachelakyerefi@icloud.com>
2026-02-23 19:02:54 +0000

    feat: set up backend portal for admin user

---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    username: { 
+        type: String, 
+        required: true 
+    },
+    isAdmin: { 
+        type: Boolean, 
+        default: false 
+    },
+    image: { 
+        type: String 
+    },
+}, { timestamps: true });
+
+// This prevents Mongoose from creating the model multiple times during Next.js hot reloads
+const User = models.User || model('User', UserSchema);
+
+export default User;
