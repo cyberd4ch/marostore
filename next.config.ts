15e2c52d1928ad614147f1d1436d682ec2ae5be4
dach-a <dachelakyerefi@icloud.com>
2026-06-13 00:43:48 +0000

    feat: latest version with a django backend

--- next.config.ts
+++ next.config.ts
@@ -18,13 +18,12 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        // Intercept any frontend call to /api/*
         source: '/api/:path*',
-        // Dynamically tunnel it to Render in production, or fallback to local Django
         destination: `${process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8000'}/api/:path*`, 
       },
     ];
   },
+
 
 
   images: {
